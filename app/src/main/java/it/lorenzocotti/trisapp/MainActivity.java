8733dbe327449ac776bf361adacbb2a6e3ff40c3
LorenzoCotti <62395571+LorenzoCotti@users.noreply.github.com>
2020-03-26 20:03:00 +0100

    Aggiunta di metodo  checkWinner() per stabilire il vincitore della partita a Triss

--- app/src/main/java/it/lorenzocotti/trisapp/MainActivity.java
+++ app/src/main/java/it/lorenzocotti/trisapp/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    private static int conta = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,43 +37,35 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) view;
         int id = button.getId();
         int cell = 0;
+        conta++;
 
         switch (id) {
 
             case R.id.b1:
-                Toast.makeText(getApplicationContext(), "b1", Toast.LENGTH_SHORT).show();
                 cell = 1;
                 break;
             case R.id.b2:
-                Toast.makeText(getApplicationContext(), "b2", Toast.LENGTH_SHORT).show();
                 cell = 2;
                 break;
             case R.id.b3:
-                Toast.makeText(getApplicationContext(), "b3", Toast.LENGTH_SHORT).show();
                 cell = 3;
                 break;
             case R.id.b4:
-                Toast.makeText(getApplicationContext(), "b4", Toast.LENGTH_SHORT).show();
                 cell = 4;
                 break;
             case R.id.b5:
-                Toast.makeText(getApplicationContext(), "b5", Toast.LENGTH_SHORT).show();
                 cell = 5;
                 break;
             case R.id.b6:
-                Toast.makeText(getApplicationContext(), "b6", Toast.LENGTH_SHORT).show();
                 cell = 6;
                 break;
             case R.id.b7:
-                Toast.makeText(getApplicationContext(), "b7", Toast.LENGTH_SHORT).show();
                 cell = 7;
                 break;
             case R.id.b8:
-                Toast.makeText(getApplicationContext(), "b8", Toast.LENGTH_SHORT).show();
                 cell = 8;
                 break;
             case R.id.b9:
-                Toast.makeText(getApplicationContext(), "b9", Toast.LENGTH_SHORT).show();
                 cell = 9;
                 break;
         }
@@ -100,63 +93,134 @@ public class MainActivity extends AppCompatActivity {
         }
 
         button.setEnabled(false);
+        checkWinner();
     }
 
     private void multiplayer() {
-        List<Integer> emptyCell = new ArrayList<>();
+        if (conta<10/2) {
+            List<Integer> emptyCell = new ArrayList<>();
 
-        for (int i=1; i<10; i++) {     //per 9 caselle
-            if(!(player1.contains(i) || player2.contains(i))) {
-                emptyCell.add(i);
+            for (int i = 1; i < 10; i++) {     //per 9 caselle
+                if (!(player1.contains(i) || player2.contains(i))) {
+                    emptyCell.add(i);
+                }
+            }
+
+            Random random = new Random();
+            int rIndex = random.nextInt(emptyCell.size());  //in base a numero celle vuote genera numero casuale all'interno di quel range di valori
+            int cellID = emptyCell.get(rIndex);  //prende quel numero
+
+            Button b = null;
+
+            switch (cellID) {
+
+                case 1:
+                    b = b1;
+                    break;
+
+                case 2:
+                    b = b2;
+                    break;
+
+                case 3:
+                    b = b3;
+                    break;
+
+                case 4:
+                    b = b4;
+                    break;
+
+                case 5:
+                    b = b5;
+                    break;
+
+                case 6:
+                    b = b6;
+                    break;
+
+                case 7:
+                    b = b7;
+                    break;
+
+                case 8:
+                    b = b8;
+                    break;
+
+                case 9:
+                    b = b9;
+                    break;
+            }
+
+            startGame(cellID, b);
+        }
+    }
+
+    private void checkWinner() {
+        int winner = 0;
+
+        //prima riga gestita
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+            winner = 1;
+        } else if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+            winner = 2;
+        }
+
+        //seconda riga gestita
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+            winner = 1;
+        } else if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+            winner = 2;
+        }
+
+        //terza riga gestita
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winner = 1;
+        } else if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winner = 2;
+        }
+
+        //prima colonna
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+            winner = 1;
+        } else if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+            winner = 2;
+        }
+
+        //seconda colonna
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+            winner = 1;
+        } else if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+            winner = 2;
+        }
+
+        //terza colonna
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winner = 1;
+        } else if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+            winner = 2;
+        }
+
+        //prima diagonale
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+            winner = 1;
+        } else if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+            winner = 2;
+        }
+
+        //seconda diagonale
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+            winner = 1;
+        } else if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+            winner = 2;
+        }
+
+        if (winner != 0) {
+            if(winner ==1) {
+                Toast.makeText(getApplicationContext(), "Ha vinto il giocatore 1!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Ha vinto il giocatore 2!", Toast.LENGTH_SHORT).show();
             }
         }
-
-        Random random = new Random();
-        int rIndex = random.nextInt(emptyCell.size());  //in base a numero celle vuote genera numero casuale all'interno di quel range di valori
-        int cellID = emptyCell.get(rIndex);  //prende quel numero
-
-        Button b = null;
-
-        switch (cellID) {
-
-            case 1:
-                b = b1;
-                break;
-
-            case 2:
-                b = b2;
-                break;
-
-            case 3:
-                b = b3;
-                break;
-
-            case 4:
-                b = b4;
-                break;
-
-            case 5:
-                b = b5;
-                break;
-
-            case 6:
-                b = b6;
-                break;
-
-            case 7:
-                b = b7;
-                break;
-
-            case 8:
-                b = b8;
-                break;
-
-            case 9:
-                b = b9;
-                break;
-        }
-
-        startGame(cellID, b);
     }
 
 }
