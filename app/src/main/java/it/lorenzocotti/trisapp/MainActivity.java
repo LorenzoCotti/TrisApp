aa5c469a96171929c3ed71db7743654b622691b7
LorenzoCotti <62395571+LorenzoCotti@users.noreply.github.com>
2020-03-26 18:20:02 +0100

    Prova debug funzionamento bottoni tramite Toast

--- app/src/main/java/it/lorenzocotti/trisapp/MainActivity.java
+++ app/src/main/java/it/lorenzocotti/trisapp/MainActivity.java
@@ -3,6 +3,8 @@ package it.lorenzocotti.trisapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickOnButton(View view) {
+        int id = view.getId();
+        int cell = 0;
+
+        switch (id) {
+
+            case R.id.b1:
+                Toast.makeText(getApplicationContext(), "b1", Toast.LENGTH_SHORT).show();
+                cell = 1;
+                break;
+            case R.id.b2:
+                Toast.makeText(getApplicationContext(), "b2", Toast.LENGTH_SHORT).show();
+                cell = 2;
+                break;
+            case R.id.b3:
+                Toast.makeText(getApplicationContext(), "b3", Toast.LENGTH_SHORT).show();
+                cell = 3;
+                break;
+            case R.id.b4:
+                Toast.makeText(getApplicationContext(), "b4", Toast.LENGTH_SHORT).show();
+                cell = 4;
+                break;
+            case R.id.b5:
+                Toast.makeText(getApplicationContext(), "b5", Toast.LENGTH_SHORT).show();
+                cell = 5;
+                break;
+            case R.id.b6:
+                Toast.makeText(getApplicationContext(), "b6", Toast.LENGTH_SHORT).show();
+                cell = 6;
+                break;
+            case R.id.b7:
+                Toast.makeText(getApplicationContext(), "b7", Toast.LENGTH_SHORT).show();
+                cell = 7;
+                break;
+            case R.id.b8:
+                Toast.makeText(getApplicationContext(), "b8", Toast.LENGTH_SHORT).show();
+                cell = 8;
+                break;
+            case R.id.b9:
+                Toast.makeText(getApplicationContext(), "b9", Toast.LENGTH_SHORT).show();
+                cell = 9;
+                break;
+        }
     }
 }
